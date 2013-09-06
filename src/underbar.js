cafcfface8902af8343601e809572fd8bf409b2e
Jeffrey C. Lee <leejeffc@gmail.com>
2013-09-06 02:57:04 -0700

    test

--- src/underbar.js
+++ src/underbar.js
@@ -115,6 +115,13 @@ var _ = { };
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var mappedArray = [];
+
+    _.each(array, function(val, index, arr) {
+      mappedArray.push(iterator(val));
+    }
+
+    return mappedArray;
   };
 
   /*
